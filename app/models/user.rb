66d97867931e98f1dd755bde59c0d3dbe6377537
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-27 14:07:40 +0530

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,19 @@ class User < ApplicationRecord
   has_many :invitations
   has_many :inverse_invitations, class_name: :Invitation, foreign_key: :friend_id
 
-
-  has_many :pending_invitations, -> { where is_accepted: false }, class_name: "Invitation", foreign_key: "user_id"
+  has_many :pending_invitations, -> { where is_accepted: false }, class_name: 'Invitation', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_invitations, source: :friend
-  has_many :inverse_pending_invitations, -> { where is_accepted: false }, class_name: "Invitation", foreign_key: "friend_id"
+  has_many :inverse_pending_invitations, -> { where is_accepted: false },
+           class_name: 'Invitation', foreign_key: 'friend_id'
   has_many :inverse_pending_friends, through: :inverse_pending_invitations, source: :user
-  has_many :accepted_invitations, -> { where is_accepted: true}, class_name: "Invitation", foreign_key: 'user_id'
+  has_many :accepted_invitations, -> { where is_accepted: true }, class_name: 'Invitation', foreign_key: 'user_id'
   has_many :friends, through: :accepted_invitations, source: :friend
 
-
   def confirm_friend(user)
-    
     confrimation = Invitation.find { |invite| invite.user == user }
     confrimation.is_accepted = true
     confrimation.save
-    revese_confirmation = self.invitations.build(friend_id: user.id, is_accepted: true)
+    revese_confirmation = invitations.build(friend_id: user.id, is_accepted: true)
     revese_confirmation.save
   end
 
